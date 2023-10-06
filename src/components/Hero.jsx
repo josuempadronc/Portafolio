b46f92be7abf5fbb1493d4358e9306389d1c06f5
Josue M Padron C <31663724+josuempadronc@users.noreply.github.com>
2023-10-06 08:37:58 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,13 +86,13 @@ const Hero = () => {
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
-          <img
+{/*           <img
             className="absolute bottom-0 ml-[50vw]
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
             src={shaq}
             alt="shaquille"
-          />
+          /> */}
         </div>
       </section>
     </>
