7b125e25037e81aa61e9ae24198635603abc1aa2
josuempadronc <josuempadron23@hotmail.com>
2023-10-05 21:49:35 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://josuempadronc.github.io/Portafolio/
 export default defineConfig({
   plugins: [react()],
+  // base: ['https://josuempadronc.github.io/Portafolio/']
 })
